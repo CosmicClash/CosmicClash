29e489c2ad7c530e8e57f64e9df101d4a47f7a2c
unknown <r202-stu@AI241LAB0202P02.aipdcls.edu>
2014-09-17 21:49:40 -0700

    Jack: Edited DataCore and changed assets

--- Assets/Scripts/DataCoreScript.cs
+++ Assets/Scripts/DataCoreScript.cs
@@ -6,116 +6,98 @@ public class DataCoreScript : MonoBehaviour
 {
 
 	//GAME OBJECTS
+	private Camera mainCamera;
 	public List<UnitClassScript> myUnits;
 
 	//Unit Selecting Variables
-	GameObject selectedUnit = null;
-	UnitClassScript selectedUnitScript = null;
+	public GameObject selectedUnit = null;
+	public UnitClassScript selectedUnitScript = null;
+	public bool isUnitSelected = false;
+
+	//For Debugging
+	private string debugString = null;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		mainCamera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		debugString = null;
+		/*IF LEFT MOUSE BUTTON DOWN*/
+		if (Input.GetMouseButtonDown(0))
+		{
+			debugString += "LMB Clicked";
+			/*CREATE RAY FROM MOUSE TO WORLD*/
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay (ray.origin, ray.direction * 5.0f, Color.yellow);
+
+			/*DETERMINE IF THE RAY HITS ANYTHING*/
+			if (Physics.Raycast (ray, out hit, 2.0f))
+			{
+				debugString += " & Ray hit object:";
+				debugString += hit.collider.gameObject.name;	
+				/*IF NO PAWN IS SELECTED*/
+				if (!isUnitSelected)
+				{
+					/*	SELECT THE PAWN HIT	*/
+					if (hit.collider.gameObject.CompareTag("unit"))
+					{
+							selectedUnit		= hit.transform.parent.gameObject;
+//							//selectedUnitScript	= selectedUnit.GetComponent(UnitClassScript);
+//							//selectedPawnScript.select();
+							isUnitSelected		= true;
+					}
+
+				}
+				/*IF A PAWN IS ALREADY SELECTED*/
+				if (isUnitSelected)
+				{
+					/*	SELECT NEW PAWN IF NEW PAWN HIT	*/
+					if (hit.collider.gameObject.CompareTag("unit") && hit.collider.gameObject != selectedPawn)
+					{
+							debugString			+= " & DESELECT CURRENT PAWN & SELECT NEW PAWN";
+							//selectedUnitScript.deselect();									//deselect current pawn
+							selectedUnit		= hit.transform.parent.gameObject;			//set new pawn into variables
+							//selectedUnitScript	= selectedPawn.GetComponent(pawnScript);	//select new pawn's script
+							//selectedUnitScript.select();									//call new pawn's select function
+					}
+					
+//					/*	SETS WAYPOINT IF GROUND IS SELECTED	*/
+//					if (hit.transform.gameObject.name == "ground")
+//					{
+//						debugString								+= " & MOVE PAWN HERE";
+//						selectedPawnScript.moveTargetSelected	= true;
+//						selectedPawnScript.moveTarget.x			= hit.point.x;
+//						selectedPawnScript.moveTarget.z			= hit.point.z;
+//					}
+//					
+//					/* IF THE USER SELECTS AN ENEMY PAWN */
+//					if (hit.transform.parent.gameObject.CompareTag("enemyPawn"))
+//					{
+//						debugString								+= " & SET THIS PAWN AS TARGET";
+//						selectedPawnScript.actionTargetSelected	= true;
+//						selectedPawnScript.actionTarget			= hit.transform.parent.gameObject;
+//						selectedPawnScript.moveTargetSelected	= true;
+//						selectedPawnScript.moveTarget.x			= hit.point.x;
+//						selectedPawnScript.moveTarget.z			= hit.point.z;
+//					}
+				}
+			}
+		}
+			}
+		}
+
+		if (debugString != null)Debug.Log (debugString);
 	}
 }
 
-//#pragma strict
-//
-///* LIST OF GAME OBJECTS */
-//var mainCamera			: Camera;
-//
-//
-///* SELECTING PAWN VARIABLES */
-//var selectedPawn		: GameObject	= null;
-//var selectedPawnScript	: pawnScript	= null;
-//var isPawnSelected		: boolean		= false;
-//var layerMask			: LayerMask;
-//
-//var debugString			: String = null;	/*	USED TO DETERMINE WHAT IS HAPPENING. Erase when finished debugging.	*/
-//
-//function Start ()
-//{
-//	mainCamera	= Camera.main;
-//}
-//
-//function Update ()
-//{
-//	debugString = null;
-//	/*	IF LEFT MOUSE BUTTON IS PRESSED	*/
-//	if (Input.GetMouseButtonDown(0))
-//	{
-//		debugString = "LMB CLICKED";
-//		/*	CREATE A RAY FROM THE MOUSE INTO THE WORLD	*/
-//		var hit : RaycastHit;
-//		var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-//		Debug.DrawRay (ray.origin, ray.direction * 100, Color.yellow);
-//		
-//		/*	DETERMINE IF THE RAY HITS ANYTHING	*/
-//		if (Physics.Raycast (ray.origin, ray.direction, hit, 10000.0, layerMask))
-//		{
-//			debugString += " & RAY HIT OBJECT";
-//			
-//			/*IF NO PAWN IS SELECTED*/
-//			if (isPawnSelected == false)
-//			{
-//				/*	SELECT THE PAWN HIT	*/
-//				if (hit.transform.parent.gameObject.CompareTag("pawn"))
-//				{
-//						debugString			+= " & HIT A PAWN";
-//						selectedPawn		= hit.transform.parent.gameObject;
-//						selectedPawnScript	= selectedPawn.GetComponent(pawnScript);
-//						selectedPawnScript.select();
-//						isPawnSelected		= true;
-//				}
-//				
-//				/*	AND YOU HIT THE GROUND	*/
-//				if (hit.transform.gameObject.name == "ground" && isPawnSelected == false)
-//				{
-//					debugString += " & HIT THE GROUND";
-//				}
-//			}
-//			
-//			/*IF A PAWN IS ALREADY SELECTED*/
-//			if (isPawnSelected == true)
-//			{
-//				/*	SELECT NEW PAWN IF NEW PAWN HIT	*/
-//				if (hit.transform.parent.gameObject.CompareTag("pawn") && hit.transform.parent.gameObject != selectedPawn)
-//				{
-//						debugString			+= " & DESELECT CURRENT PAWN & SELECT NEW PAWN";
-//						selectedPawnScript.deselect();									//deselect current pawn
-//						selectedPawn		= hit.transform.parent.gameObject;			//set new pawn into variables
-//						selectedPawnScript	= selectedPawn.GetComponent(pawnScript);	//select new pawn's script
-//						selectedPawnScript.select();									//call new pawn's select function
-//				}
-//				
-//				/*	SETS WAYPOINT IF GROUND IS SELECTED	*/
-//				if (hit.transform.gameObject.name == "ground")
-//				{
-//					debugString								+= " & MOVE PAWN HERE";
-//					selectedPawnScript.moveTargetSelected	= true;
-//					selectedPawnScript.moveTarget.x			= hit.point.x;
-//					selectedPawnScript.moveTarget.z			= hit.point.z;
-//				}
-//				
-//				/* IF THE USER SELECTS AN ENEMY PAWN */
-//				if (hit.transform.parent.gameObject.CompareTag("enemyPawn"))
-//				{
-//					debugString								+= " & SET THIS PAWN AS TARGET";
-//					selectedPawnScript.actionTargetSelected	= true;
-//					selectedPawnScript.actionTarget			= hit.transform.parent.gameObject;
-//					selectedPawnScript.moveTargetSelected	= true;
-//					selectedPawnScript.moveTarget.x			= hit.point.x;
-//					selectedPawnScript.moveTarget.z			= hit.point.z;
-//				}
-//			}
-//		}
-//	}
+
+
 //	
 //	/*	IF RIGHT MOUSE BUTTON IS PRESSED	*/
 //	if (Input.GetMouseButtonDown(1))
